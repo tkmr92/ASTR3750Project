4fad1ddc1b8bd458b608fd6b28af3429f8537593
Tarek <tarek.mackler@colorado.edu>
2016-11-09 16:11:08 -0700

    did stuff

--- python/cratering.py
+++ python/cratering.py
@@ -1,4 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as pl
 
+x = np.zeros(500)
+y = np.zeros(500)
+intensity = np.array(np.zeros(3))
+
+np.zip(x, y, intensity)
+
+for things in stuff:
+    do magic
+
+show magic
 
